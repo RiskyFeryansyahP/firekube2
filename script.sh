d5a76216314543eb033a53bb3479ae9953b24e62
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-05-18 19:09:11 +0700

    change

--- script.sh
+++ script.sh
@@ -4,13 +4,6 @@
 installDockerCentos() {
     echo "Installing docker in centos ...."
 
-    # Uninstall old versions
-   sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
-
-    # SET UP THE REPOSITORY
-    sudo yum install -y yum-utils
-    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
     # INSTALL DOCKER ENGINE
     sudo yum -y install docker-ce docker-ce-cli containerd.io
     
