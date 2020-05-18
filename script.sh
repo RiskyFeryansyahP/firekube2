dc8dc389d42afcb50fe287e4b35f6264591e596f
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-05-18 19:31:21 +0700

    fix

--- script.sh
+++ script.sh
@@ -7,49 +7,43 @@ installDockerCentos() {
     if [[ $(which docker) && $(docker --version) ]]; then
         echo "docker has been installed!"
     else
-        echo "docker not installed!"
+        # Uninstall old versions
+        sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+
+        # SET UP THE REPOSITORY
+        sudo yum install -y yum-utils
+        sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+        # INSTALL DOCKER ENGINE
+        sudo yum -y install docker-ce docker-ce-cli containerd.io
+        
+        # Check Docker Version
+        docker version
     fi
-
-    # Uninstall old versions
-#    sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
-
-#     # SET UP THE REPOSITORY
-#     sudo yum install -y yum-utils
-#     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
-#     # INSTALL DOCKER ENGINE
-#     sudo yum -y install docker-ce docker-ce-cli containerd.io
-    
-#     # Check Docker Version
-#     docker version
 }
 
 # Install kubectl
 installKubectl() {
     if [ $(which kubectl) ]; then
-        echo "kubectl installed!"
+        echo "kubectl has been installed!"
     else
-        echo "kubectl not installed!"
+        curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+        chmod +x ./kubectl
+        sudo mv ./kubectl /usr/local/bin/kubectl
+        kubectl version --client
     fi
-
-    # curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-    # chmod +x ./kubectl
-    # sudo mv ./kubectl /usr/local/bin/kubectl
-    # kubectl version --client
 }
 
 # Install wksctl
 installWksctl() {
     if [ $(wksctl version) ]; then
-        echo "wksctl installed"
+        echo "wksctl has been installed"
     else
-        echo "wksctl not installed"
+        curl -LO https://github.com/weaveworks/wksctl/releases/download/0.8.1/wksctl-0.8.1-linux-x86_64.tar.gz
+        tar xfz wksctl-0.8.1-linux-x86_64.tar.gz
+        chmod +x wksctl
+        sudo mv wksctl /usr/local/bin
     fi
-
-#     curl -LO https://github.com/weaveworks/wksctl/releases/download/0.8.1/wksctl-0.8.1-linux-x86_64.tar.gz
-#     tar xfz wksctl-0.8.1-linux-x86_64.tar.gz
-#     chmod +x wksctl
-#     sudo mv wksctl /usr/local/bin
 }
 
 # Check update centos
